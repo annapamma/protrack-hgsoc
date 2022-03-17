cafe7cef66fa95406c3c8892bc0fcea448d39e4c
Anna Calinawan <anna@Annas-MBP.lan>
2022-03-17 13:26:47 -0400

    Adds multiple levels of sorting

--- src/components/HeatmapSorter.vue
+++ src/components/HeatmapSorter.vue
@@ -7,6 +7,7 @@
             :track="track" 
             :i="i"
             @updateLockTrack="updateLockTrack"
+            @removeLockTrack="removeLockTrack"
         />
         <!-- <v-autocomplete
             v-model="selectTrack"
@@ -17,6 +18,30 @@
             prepend-icon="mdi-plus"
             append-icon=""
         ></v-autocomplete> -->
+        <div class="add-button">
+            <v-btn
+                color="indigo lighten-4"
+                hint="test"
+                small
+                class="mt-2"
+                prepend-icon="mdi-plus"
+                @click="addLockTrack"
+            >
+                <v-icon>mdi-plus</v-icon>
+            </v-btn>
+            <span class="add-button-label">Add another level of ordering</span>
+        </div>
+        <div style=" text-align: center;">
+            <v-btn
+                class="mt-4 mx-2"
+                color="primary"
+                :disabled="lockTracks.length === 0 || !changed"
+                @click="sortTracks"
+            >
+                <span v-if="lockTracks.length > 0">Apply changes</span>
+                <span v-else>Select at least 1<br> axis to order</span>
+            </v-btn>
+        </div>
   </div>
 </template>
 
@@ -30,68 +55,88 @@ export default {
     name: "heatmap-sorter",
 
     computed: {
-        heatmapLockTracks: {
-            get() { return this.$store.state.heatmapLockTracks },
-            set(heatmapLockTracks) { this.$store.dispatch('setHeatmapLockTracks', { heatmapLockTracks }) },
-        },
-        heatmapTracksToLock() { return this.$store.state.heatmapTracksToLock },
-        gene() { return this.selectedSeries ? this.selectedSeries.split(' ')[0] : null },
-        selectedSample() { return this.$store.state.selectedSample },
-        selectedSeries() { return this.$store.state.selectedSeries },
-        selectedValue() { return this.$store.state.selectedValue },
-        selectedData() { return [
-            ['Selected series', this.selectedSeries],
-            ['Selected sample', this.selectedSample],
-            ['Selected value', this.selectedValue],
-        ] },
-        heatmapTracks() { return Object.keys(this.$store.state.trackDetails) },
+        // heatmapLockTracks: {
+        //     get() { return this.$store.state.heatmapLockTracks },
+        //     set(heatmapLockTracks) { this.$store.dispatch('setHeatmapLockTracks', { heatmapLockTracks }) },
+        // },
+        // heatmapTracksToLock() { return this.$store.state.heatmapTracksToLock },
+        // gene() { return this.selectedSeries ? this.selectedSeries.split(' ')[0] : null },
+        // selectedSample() { return this.$store.state.selectedSample },
+        // selectedSeries() { return this.$store.state.selectedSeries },
+        // selectedValue() { return this.$store.state.selectedValue },
+        // selectedData() { return [
+        //     ['Selected series', this.selectedSeries],
+        //     ['Selected sample', this.selectedSample],
+        //     ['Selected value', this.selectedValue],
+        // ] },
+        // heatmapTracks() { return Object.keys(this.$store.state.trackDetails) },
     },
 
     data: () => ({
         lockTracks: [],
-        selectTrack: '',
-        sortTracks: [],
-        sortButtons: {
-            asc: {
-                label: 'Asc',
-                icon: 'mdi-arrow-up-bold',
-                asc: true,
-            },
-            desc: {
-                label: 'Desc',
-                icon: 'mdi-arrow-down-bold',
-                asc: false,
-            },
-        }
+        changed: false,
+        // selectTrack: '',
+        // sortTracks: [],
+        // sortButtons: {
+        //     asc: {
+        //         label: 'Asc',
+        //         icon: 'mdi-arrow-up-bold',
+        //         asc: true,
+        //     },
+        //     desc: {
+        //         label: 'Desc',
+        //         icon: 'mdi-arrow-down-bold',
+        //         asc: false,
+        //     },
+        // }
     }),
 
     watch: {
-        selectTrack() { this.addTrack() },
-        lockTracks() { 
-            console.log('updated lock track')
-            // const rev = [...this.lockTracks]
-            // rev.reverse()
-            // this.$store.dispatch('setHeatmapTracksToLock', { heatmapTracksToLock: rev })
-        },
+        // selectTrack() { this.addTrack() },
+        // lockTracks() { 
+        //     if (this.lockTracks.length > 0) {
+        //         this.changed = true
+        //     }
+        //     // console.log('updated lock track')
+        //     // const rev = [...this.lockTracks]
+        //     // rev.reverse()
+        //     // this.$store.dispatch('setHeatmapTracksToLock', { heatmapTracksToLock: rev })
+        // },
 
     },
 
     methods: {
-        lockIdx(i) { return this.lockTracks.length > 1 ? i + 1 : '' },
-        sortBySeries({ asc }) {
-            this.$store.dispatch('sortSamples', { asc, series: this.selectedSeries })
+        addLockTrack() {
+            let lockTracks = [...this.lockTracks]
+            lockTracks.push('Search tracks')
+            this.lockTracks = lockTracks
+            this.changed = true
         },
-        removeTrack(track) { 
-            const res = this.lockTracks.filter(el => el !== track) 
-            this.lockTracks = res
-        },
-        addTrack() { 
-            this.lockTracks.push(this.selectTrack) 
+        // lockIdx(i) { return this.lockTracks.length > 1 ? i + 1 : '' },
+        // sortBySeries({ asc }) {
+        //     this.$store.dispatch('sortSamples', { asc, series: this.selectedSeries })
+        // },
+        // removeTrack(track) { 
+        //     const res = this.lockTracks.filter(el => el !== track) 
+        //     this.lockTracks = res
+        // },
+        // addTrack() { 
+        //     this.lockTracks.push(this.selectTrack) 
+        // },
+        removeLockTrack({ i }) {
+            let lockTracks = [...this.lockTracks]
+            lockTracks.splice(i, 1)
+            this.lockTracks = lockTracks
+            this.changed = true
         },
         updateLockTrack({ i, updatedTrack }) {
             let lockTracks = [...this.lockTracks]
             lockTracks[i] = updatedTrack
             this.lockTracks = lockTracks
+            this.changed = true
+        },
+        sortTracks() {
+            console.log('sort')
         },
     },
 
@@ -107,6 +152,16 @@ export default {
 .heatmap-sorter {
     width: 100%;
     margin-top: 10px;
+}
+
+.add-button {
+    display: flex;
+    flex-direction: row;
+}
+
+.add-button-label {
+    margin-top: 10px;
+    margin-left: 4px;
 }
 
 .sort-buttons-container {
--- src/components/HeatmapSorterLevel.vue
+++ src/components/HeatmapSorterLevel.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="heatmap-sorter-level">
-
         <div v-if="!editing">{{ updatedTrack }}</div>
         <div v-else>
             <v-autocomplete
@@ -17,6 +16,7 @@
                 x-small
                 @click="editing = true"
                 style="margin-left: 10px;"
+                hint="test"
             >
                 <v-icon>mdi-pencil</v-icon>
             </v-btn>
@@ -24,12 +24,21 @@
         <div v-else>
             <v-btn
                 icon
-                color="grey"
-                x-small
+                color="green"
+                small
                 @click="updateLockTrack"
                 style="margin-left: 10px;"
             >
                 <v-icon>mdi-check</v-icon>
+            </v-btn>
+            <v-btn
+                icon
+                color="red"
+                small
+                @click="removeLockTrack"
+                style="margin-left: 10px;"
+            >
+                <v-icon>mdi-delete</v-icon>
             </v-btn>
         </div>
     </div>
@@ -60,20 +69,42 @@ export default {
     },
 
     methods: {
-        updateLockTrack() {
+        removeLockTrack() {
             this.editing = false
             this.$emit(
-                'updateLockTrack', 
+                'removeLockTrack', 
                 { 
-                    updatedTrack: this.updatedTrack,
                     i: this.i,
                 }
             )
+        },
+        updateLockTrack() {
+            this.editing = false
+            if (this.updatedTrack === 'Search tracks') {
+                this.$emit(
+                    'removeLockTrack', 
+                    { 
+                        i: this.i,
+                    }
+                )
+            } else {
+                this.$emit(
+                    'updateLockTrack', 
+                    { 
+                        updatedTrack: this.updatedTrack,
+                        i: this.i,
+                    }
+                )
+
+            }
         },
     },
 
     mounted() { 
         this.updatedTrack = this.track
+        if (this.track === 'Search tracks') {
+            this.editing = true
+        }
     },
 }
 </script>
@@ -83,6 +114,9 @@ export default {
     width: 100%;
     display: flex;
     flex-direction: row;
+    border: solid 1px #ebebeb;
+    padding: 2px 10px;
+    justify-content: space-between;
 }
 
 .heatmap-sorter-level span {
