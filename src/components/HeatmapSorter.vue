299f08d162f1187ba6acce33659de9f3a08c2074
Anna Calinawan <anna@Annas-MBP.lan>
2022-03-20 15:41:36 -0400

    Remove unused comments

--- src/components/HeatmapSorter.vue
+++ src/components/HeatmapSorter.vue
@@ -8,15 +8,6 @@
             @updateLockTrack="updateLockTrack"
             @removeLockTrack="removeLockTrack"
         />
-        <!-- <v-autocomplete
-            v-model="selectTrack"
-            :items="heatmapTracks.filter(track => !lockTracks.includes(track))"
-            xSmall
-            label="Add another level of ordering"
-            hide-details
-            prepend-icon="mdi-plus"
-            append-icon=""
-        ></v-autocomplete> -->
         <div class="add-button">
             <v-btn
                 color="indigo lighten-4"
@@ -53,56 +44,10 @@ export default {
     },
     name: "heatmap-sorter",
 
-    computed: {
-        // heatmapLockTracks: {
-        //     get() { return this.$store.state.heatmapLockTracks },
-        //     set(heatmapLockTracks) { this.$store.dispatch('setHeatmapLockTracks', { heatmapLockTracks }) },
-        // },
-        // heatmapTracksToLock() { return this.$store.state.heatmapTracksToLock },
-        // gene() { return this.selectedSeries ? this.selectedSeries.split(' ')[0] : null },
-        // selectedSample() { return this.$store.state.selectedSample },
-        // selectedSeries() { return this.$store.state.selectedSeries },
-        // selectedValue() { return this.$store.state.selectedValue },
-        // selectedData() { return [
-        //     ['Selected series', this.selectedSeries],
-        //     ['Selected sample', this.selectedSample],
-        //     ['Selected value', this.selectedValue],
-        // ] },
-        // heatmapTracks() { return Object.keys(this.$store.state.trackDetails) },
-    },
-
     data: () => ({
         lockTracks: [],
         changed: false,
-        // selectTrack: '',
-        // sortTracks: [],
-        // sortButtons: {
-        //     asc: {
-        //         label: 'Asc',
-        //         icon: 'mdi-arrow-up-bold',
-        //         asc: true,
-        //     },
-        //     desc: {
-        //         label: 'Desc',
-        //         icon: 'mdi-arrow-down-bold',
-        //         asc: false,
-        //     },
-        // }
     }),
-
-    watch: {
-        // selectTrack() { this.addTrack() },
-        // lockTracks() { 
-        //     if (this.lockTracks.length > 0) {
-        //         this.changed = true
-        //     }
-        //     // console.log('updated lock track')
-        //     // const rev = [...this.lockTracks]
-        //     // rev.reverse()
-        //     // this.$store.dispatch('setHeatmapTracksToLock', { heatmapTracksToLock: rev })
-        // },
-
-    },
 
     methods: {
         addLockTrack() {
@@ -110,17 +55,6 @@ export default {
             lockTracks.push('Search tracks')
             this.lockTracks = lockTracks
         },
-        // lockIdx(i) { return this.lockTracks.length > 1 ? i + 1 : '' },
-        // sortBySeries({ asc }) {
-        //     this.$store.dispatch('sortSamples', { asc, series: this.selectedSeries })
-        // },
-        // removeTrack(track) { 
-        //     const res = this.lockTracks.filter(el => el !== track) 
-        //     this.lockTracks = res
-        // },
-        // addTrack() { 
-        //     this.lockTracks.push(this.selectTrack) 
-        // },
         removeLockTrack({ i }) {
             let lockTracks = [...this.lockTracks]
             lockTracks.splice(i, 1)
